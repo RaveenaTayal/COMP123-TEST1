7276033b83bd084e6b707e604c6489ff7a36a767
RaveenaTayal <ravhome123@gmail.com>
2015-02-04 09:18:55 -0500

    Created a car class

---
+++ COMP123-TEST1/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_TEST1
+{
+    class Car
+    {
+        //----------------------------------PRIVATE PROPERTIES--------------------------------------
+        private string Make;
+        private string Model;
+        private string Year;
+        private string Colour;
+
+        //----------------------------------CONSTRUCTOR---------------------------------------------
+        public Car(string Make,string Model,string Year,string Colour)
+        {
+            //Initialising instance variables
+            this.Make=Make;
+            this.Model=Model;
+            this.Year=Year;
+            this.Colour=Colour;
+            this.show();
+        }
+        //----------------------------------PUBLIC METHODS------------------------------------------
+        public void drive()
+        {
+            Console.WriteLine(" The car is Driving");
+            Console.WriteLine();
+        }
+
+        public void show()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Wanna know about your Car?????");
+            Console.WriteLine(" Here's it !!!!!");
+            Console.WriteLine(" ------------------");
+            Console.WriteLine(" Make: {0}", Make);
+            Console.WriteLine(" Model: {0}", Model);
+            Console.WriteLine(" Year: {0}",Year );
+            Console.WriteLine(" Colour: {0}", Colour);
+            Console.WriteLine(" ------------------");
+            Console.WriteLine();
+        }
+    }
+}
