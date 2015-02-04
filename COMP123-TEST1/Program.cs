31f84d125cdcd4fbed90e9ea5d14ae3bef937843
RaveenaTayal <ravhome123@gmail.com>
2015-02-04 09:21:07 -0500

    Implemented Car class by instantiating objects for it

---
+++ COMP123-TEST1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_TEST1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating an object for Car class
+            Car myCar = new Car("Ford","Ultimate","2011","Grey");
+
+            //Calling drive() method
+            myCar.drive();
+
+            //To show the output on Console without crashing
+            Console.WriteLine(" Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
